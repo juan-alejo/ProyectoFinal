e6c89344abbb450c16e428706e17d5b5bbf292d5
juan-alejo <juanalejofarana@gmail.com>
2022-06-22 05:32:44 -0300

    formulario servidor

--- ProyectoFinalApp/urls.py
+++ ProyectoFinalApp/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     # URLS de la app
     path('', inicio, name="inicio"),
-    path('servidores/', servidores, name= "servidores")
-
+    path('servidores/', servidores, name= "servidores"),
+    path('crearservidor/', crearServidor, name= "crearServidor"),
     #path('profesores/', profesores, name="profesores"),
 ]
--- ProyectoFinalApp/views.py
+++ ProyectoFinalApp/views.py
@@ -22,3 +22,34 @@ def servidores(request):
     servidores = Servidor.objects.all()
     
     return render(request,"ProyectoFinalApp/servidores.html",{"servidores":servidores})
+
+
+def crearServidor(request):
+ 
+    #post
+    if request.method == "POST":
+        
+        formularioServidor = NuevoServidor(request.POST)
+        
+        if formularioServidor.is_valid():
+            
+            infoServidor = formularioServidor.cleaned_data
+        
+            servidor = Servidor(nombre = infoServidor["nombre"], version =int(infoServidor["version"]))
+            
+            servidor.save()
+            
+            return redirect("servidores") 
+        
+        else:
+            return render(request,"ProyectoFinalApp/formularioServidor.html",{"form":formularioVacio})
+    
+    
+    else: #get y otros
+        
+        formularioVacio = NuevoServidor()
+        
+        
+        return render(request,"ProyectoFinalApp/formularioServidor.html",{"form":formularioVacio})
+        
+   # return HttpResponse("asd")
