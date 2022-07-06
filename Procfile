8a285eb50ba197e98fdef070b764e07349cbe030
juan-alejo <juanalejofarana@gmail.com>
2022-07-05 22:48:47 -0300

    deploying heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath ProyectoFinal ProyectoFinal.wsgi
--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +84,9 @@ WSGI_APPLICATION = 'ProyectoFinal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -90,6 +94,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
